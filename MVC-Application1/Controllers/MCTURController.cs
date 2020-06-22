243e166dce1978634a3f4632dae3f21048b3020d
skype <skype@CLIENT>
2020-06-22 11:24:27 -0500

    Primer cambio (Antes de cargar los datos)

---
+++ MVC-Application1/Controllers/MCTURController.cs
@@ -0,0 +1,22 @@
+﻿using MVC_Application1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Application1.Controllers
+{
+    public class MCTURController : Controller
+    {
+        // GET: MCTUR
+        public ActionResult Index()
+        {
+            MCTURClass1 obj = new MCTURClass1();
+            obj.edad = 29;
+            obj.nombre = "Alejandro";
+            obj.apellido = "Reyes";
+            return View(obj);
+        }
+    }
+}
---
+++ MVC-Application1/Models/MCTURClass1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Application1.Models
+{
+    public class MCTURClass1
+    {
+        public int edad { get; set; }
+        public String nombre { get; set; }
+        public String apellido { get; set; }
+    }
+}
